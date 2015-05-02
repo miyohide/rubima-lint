dd0c854580543e408b5e25991c3b53eb0e29f5a9
miyohide <miyohide@gmail.com>
2015-05-02 21:13:00 +0900

    initial commit

---
+++ lib/rubima/lint.rb
@@ -0,0 +1,7 @@
+require "rubima/lint/version"
+
+module Rubima
+  module Lint
+    # Your code goes here...
+  end
+end
---
+++ lib/rubima/lint/version.rb
@@ -0,0 +1,5 @@
+module Rubima
+  module Lint
+    VERSION = "0.1.0"
+  end
+end
